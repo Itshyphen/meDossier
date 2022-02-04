f420f5050f80c1a8b82bfc312872f3dade24b4ec
anupamakoirala <anukoirala.96@hotmail.com>
2022-02-04 22:12:17 +0545

    Remove node_modules folder

--- frontend/src/moralis/node.js
+++
@@ -1,7 +0,0 @@
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    plugins: [new NodePolyfillPlugin()],
-  });
-};
--- migrations/2_deploy_contract.js
+++
@@ -1,5 +0,0 @@
-const MeDossier = artifacts.require("meDossier");
-
-module.exports = function (deployer) {
-  deployer.deploy(MeDossier);
-};
